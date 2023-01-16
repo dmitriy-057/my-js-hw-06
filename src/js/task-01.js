f885ff285669ddf3d061d3b2ca27d499aea02443
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-01-17 00:12:04 +0200

    add task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,23 @@
+// 1 - получить доступ к списку --> ul#categories;
 const categoriesEl = document.querySelector('#categories');
-console.log('Number of categories:',categoriesEl.children.length);
+
+// 2 - получить массиа всех <li> в списке и их длину;
+const allItemsOfCategories = categoriesEl.querySelectorAll('.item');
+
+console.log('Numbers of Categories:', allItemsOfCategories.length);
+
+// 3 - перебрать массив категорий методом forEach;
+    // 3.1 - получить доступ к тегу <h2> в каждой категории;
+    // 3.2 - получить доступ к списку каждой категории;
+    // 3.3 - узнать длину дочерних элементов такого списка;
+allItemsOfCategories.forEach(item => {
+    const itemTitle = item.querySelector('h2');
+    console.log('Category', itemTitle.textContent);
+
+    const categoryList = item.querySelector('ul');
+    
+    const itemsOfCategoryList = categoryList.children;
+    console.log('Elements', itemsOfCategoryList.length)
+    
+})
 
