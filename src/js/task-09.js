a5786f7cc22f1ea2c42d1ff121e8a2de3b114632
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-01-19 23:09:05 +0200

    add task 10

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
+const btnEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
+
+btnEl.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  console.log(bodyEl.style.backgroundColor)
+
+  spanEl.textContent = bodyEl.style.backgroundColor;
+}
